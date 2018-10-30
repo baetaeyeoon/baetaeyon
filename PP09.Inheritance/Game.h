f16188da9496a9ea479bdf6da82f10418e5d7b67
starr <starrain0273@naver.com>
2018-10-30 20:50:14 +0900

    20181030

---
+++ PP09.Inheritance/Game.h
@@ -0,0 +1,29 @@
+#include "TextureManager.h"
+#include "Player.h"
+#include "GameObject.h"
+
+GameObject m_go;
+Player m_player;
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+	int m_currentFrame;
+	TextureManager m_textureManager;
+private:
+	SDL_Texture * m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+protected:
+};
